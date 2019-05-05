0aae46a56d0beff0332df04cfb14cb53542de19e
Avitosh Totaram <a.totaram@gmail.com>
2019-05-05 23:21:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 # Add  code here!
 def prime?(num)
-  if num > 2 && num%2 != 0 || num%3 != 0 
-    puts "Your Integer is a Prime Number"
+  if num == 1 || num = -1 || num == 2 || num = -2 || num == 3 || num = -3 || num == 1 || num = -1 || 
+    
+    else if num % 2 != 0 || num%3 != 0 
+    puts "true"
   end
 end
