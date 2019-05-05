e8a2d72a9a205f31c779c04c83f2ff9f9bc6fd80
Avitosh Totaram <a.totaram@gmail.com>
2019-05-05 23:26:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,15 @@
-def prime?(number)
-  if number >= 2
-    (2..number - 1).all? do |x|
-      number % x != 0
-      return true
+def prime?(num)
+  # check if num is less than 2.
+  false if num < 2
+  
+  # check if num is divisible by any number
+  # between 2 to (num - 1)
+  # return false on the first instance
+  (2...num).each do |factor|
+    if (num % factor).zero? 
+      return false
     end
-  else
-    return false
   end
+  
+  true # after checking all numbers and not receiving false
 end
